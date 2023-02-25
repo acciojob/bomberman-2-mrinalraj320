3072165a97b1f7c3a2e113a9378f22a50dad57d8
Mrinal Raj <120112180+mrinalraj320@users.noreply.github.com>
2023-02-25 22:20:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,4 +16,29 @@ document.getElementById('change_button').addEventListener('click', () => {
     // alert(colorId)
     const block = document.getElementById(`${blockId}`);
     block.style.backgroundColor = color;
+	let bombIds = [];
+while (bombIds.length < 10) {
+    let randomId = Math.floor(Math.random() * 60) + 20;
+@@ -231,7 +213,6 @@ for (let i = 0; i < bombIds.length; i++) {
+    }
+}
+
+//============revelaing boxes===============//
+
+let boardDivs = document.querySelectorAll('.grid-item')
+boardDivs.forEach(boardCell => {
+@@ -252,7 +233,6 @@ function revealingDivs(e) {
+        clickedCell.style.opacity = '0.6'
+        clickedCell.classList.add('checked')
+
+        //====displaying the number of bobms==//
+        if (!clickedCell.classList.contains('bomb')) {
+            let bombCount = 0;
+            console.log("first after declaration", bombCount);
+@@ -515,4 +495,4 @@ function won() {
+    boardDivs.forEach(boardCell => {
+        boardCell.removeEventListener("contextmenu", addingFlag)
+    })
+}
+}
 });
